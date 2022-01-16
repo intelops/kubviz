342036a42d0fcc9a8183079818cb275f00297beb
Avinash K N <avinashkna4@gmail.com>
2022-01-16 18:34:32 +0530

    Add event model

---
+++ model/db_event.go
@@ -0,0 +1,21 @@
+package model
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type DbEvent struct {
+	Id         uuid.UUID
+	Op_type    string
+	Name       string
+	Namespace  string
+	Kind       string
+	Message    string
+	Host       string
+	Event      string
+	First_time time.Time
+	Last_time  time.Time
+	Event_time time.Time
+}
