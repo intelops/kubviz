33b4af2859fa864ce014c22b215fb7daec232161
ahinvinith <ahinfencer07@gmail.com>
2023-06-01 13:04:45 +0530

    Modified dashboard for ketall and outdated plugins

--- model/ketall.go
+++ model/ketall.go
@@ -2,6 +2,7 @@ package model
 
 type Resource struct {
 	Resource    string `json:"resource"`
+	Kind        string `json:"kind"`
 	Namespace   string `json:"namespace"`
 	Age         string `json:"age"`
 	ClusterName string `json:"clustername"`
