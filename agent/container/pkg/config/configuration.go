90b1fb03d0024eb4557c453318d1f145c4deaf5f
vijeyash <vijeyash@gmail.com>
2023-08-01 10:50:32 +0530

    added GithubConfig to get organisation and github token

--- agent/container/pkg/config/configuration.go
+++ agent/container/pkg/config/configuration.go
@@ -3,10 +3,15 @@ package config
 // var token string = "UfmrJOYwYCCsgQvxvcfJ3BdI6c8WBbnD"
 // var natsurl string = "nats://localhost:4222"
 
-//Config will have the configuration details
+// Config will have the configuration details
 type Config struct {
 	NatsAddress string `envconfig:"NATS_ADDRESS"`
 	NatsToken   string `envconfig:"NATS_TOKEN"`
 	Port        int    `envconfig:"PORT"`
 	StreamName  string `envconfig:"STREAM_NAME"`
+}
+
+type GithubConfig struct {
+	Org   string `envconfig:"GITHUB_ORG"`
+	Token string `envconfig:"GITHUB_TOKEN"`
 }
