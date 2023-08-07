a946cce49cc76cacf4923f35b5801cbd6665d048
Nithunikzz <nithunithin2000@gmail.com>
2023-08-07 16:35:25 +0530

    changes in kubescore

--- agent/kubviz/kube_score.go
+++ agent/kubviz/kube_score.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"context"
 	"encoding/json"
 	"log"
 	exec "os/exec"
@@ -12,69 +11,55 @@ import (
 	"github.com/intelops/kubviz/model"
 	"github.com/nats-io/nats.go"
 	"github.com/zegl/kube-score/renderer/json_v2"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 )
 
 func RunKubeScore(clientset *kubernetes.Clientset, js nats.JetStreamContext, wg *sync.WaitGroup, errCh chan error) {
 	defer wg.Done()
-
-	nsList, err := clientset.CoreV1().
-		Namespaces().
-		List(context.Background(), metav1.ListOptions{})
-	if err != nil {
-		log.Println("Error occurred while getting client set for kube-score: ", err)
-		return
-	}
-
-	log.Printf("Namespace size: %d", len(nsList.Items))
-	var wgNamespaces sync.WaitGroup
-	for _, n := range nsList.Items {
-		wgNamespaces.Add(1)
-		log.Printf("Publishing kube-score recommendations for namespace: %s\n", n.Name)
-		go publish(n.Name, js, &wgNamespaces, errCh)
-	}
-}
-
-func publish(ns string, js nats.JetStreamContext, wg *sync.WaitGroup, errCh chan error) {
-	defer wg.Done()
 	var report []json_v2.ScoredObject
-
-	cmd := "kubectl api-resources --verbs=list --namespaced -o name | xargs -n1 -I{} sh -c \"kubectl get {} -n " + ns + " -oyaml && echo ---\" | kube-score score - -o json"
+	cmd := "kubectl api-resources --verbs=list --namespaced -o name | xargs -n1 -I{} sh -c \"kubectl get {} --all-namespaces -oyaml && echo ---\" | kube-score score - -o json"
 	log.Printf("Command:  %#v,", cmd)
 	out, err := executeCommand(cmd)
 
+	if err != nil {
+		log.Printf("Error scanning image %s:", err)
+		//continue // Move on to the next image in case of an error
+	}
+
 	err = json.Unmarshal([]byte(out), &report)
+
 	if err != nil {
 		log.Printf("Error occurred while Unmarshalling json: %v", err)
-		errCh <- err
+		//continue // Move on to the next image in case of an error
 	}
+	publishKubescoreMetrics(report, js, errCh)
+	//log.Println("Publishing kube-score recommendations for namespace")
 
-	if err != nil {
-		log.Println("Error occurred while running kube-score: ", err)
-		errCh <- err
-	}
-	err = publishKubescoreMetrics(uuid.New().String(), report, js)
-	if err != nil {
-		errCh <- err
-	}
-	errCh <- nil
+	// If you want to publish the report or perform any other action with it, you can do it here
+
 }
 
-func publishKubescoreMetrics(id string, report []json_v2.ScoredObject, js nats.JetStreamContext) error {
+func publishKubescoreMetrics(report []json_v2.ScoredObject, js nats.JetStreamContext, errCh chan error) {
 	metrics := model.KubeScoreRecommendations{
-		ID:          id,
+		ID:          uuid.New().String(),
 		ClusterName: ClusterName,
 		Report:      report,
 	}
-	metricsJson, _ := json.Marshal(metrics)
-	_, err := js.Publish(constants.KUBESCORE_SUBJECT, metricsJson)
+	metricsJson, err := json.Marshal(metrics)
 	if err != nil {
-		return err
+		log.Printf("Error marshaling metrics to JSON: %v", err)
+		errCh <- err
+		return
 	}
-	log.Printf("Recommendations with ID:%s has been published\n", id)
-	log.Printf("Recommendations  :%#v", report)
-	return nil
+
+	_, err = js.Publish(constants.KUBESCORE_SUBJECT, metricsJson)
+	if err != nil {
+		log.Printf("error occures while publish %v", err)
+		errCh <- err
+		return
+	}
+	log.Printf("KubeScore report with ID:%s has been published\n", metrics.ID)
+	errCh <- nil
 }
 
 func executeCommand(command string) (string, error) {
