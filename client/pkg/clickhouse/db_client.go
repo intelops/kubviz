4ee420b1b5c16852c839a0d82854587530fd1e13
an1l4 <1995anila@gmail.com>
2023-08-14 23:42:57 +0530

    reduced for loop

--- client/pkg/clickhouse/db_client.go
+++ client/pkg/clickhouse/db_client.go
@@ -434,54 +434,48 @@ func (c *DBClient) InsertTrivyImageMetrics(metrics model.TrivyImage) {
 }
 func (c *DBClient) InsertTrivySbomMetrics(metrics model.Reports) {
 	result := metrics.Report
-	for _, metaTool := range result.Metadata.Tools {
-		for _, com := range result.Components {
-			for _, comPro := range com.Properties {
-				for _, comHash := range com.Hashes {
-					for _, comLicense := range com.Licenses {
-						for _, depend := range result.Dependencies {
-							var (
-								tx, _   = c.conn.Begin()
-								stmt, _ = tx.Prepare(InsertTrivySbom)
-							)
-							if _, err := stmt.Exec(
-								metrics.ID,
-								result.Schema,
-								result.BomFormat,
-								result.SpecVersion,
-								result.SerialNumber,
-								result.Version,
-								result.Metadata.Timestamp,
-								metaTool.Vendor,
-								metaTool.Name,
-								metaTool.Version,
-								com.BomRef,
-								com.Type,
-								com.Name,
-								com.Version,
-								comPro.Name,
-								comPro.Value,
-								comHash.Alg,
-								comHash.Content,
-								comLicense.Expression,
-								com.Purl,
-								depend.Ref,
-							); err != nil {
-								log.Fatal(err)
-							}
-							if err := tx.Commit(); err != nil {
-								log.Fatal(err)
-							}
-							stmt.Close()
-						}
-					}
-
-				}
+	for _, com := range result.Components {
+		if len(result.Metadata.Tools) == 0 || len(com.Properties) == 0 || len(com.Hashes) == 0 || len(com.Licenses) == 0 {
+			continue
+		}
+		for _, depend := range result.Dependencies {
+			var (
+				tx, _   = c.conn.Begin()
+				stmt, _ = tx.Prepare(InsertTrivySbom)
+			)
+			if _, err := stmt.Exec(
+				metrics.ID,
+				result.Schema,
+				result.BomFormat,
+				result.SpecVersion,
+				result.SerialNumber,
+				result.Version,
+				result.Metadata.Timestamp,
+				result.Metadata.Tools[0].Vendor,
+				result.Metadata.Tools[0].Name,
+				result.Metadata.Tools[0].Version,
+				com.BomRef,
+				com.Type,
+				com.Name,
+				com.Version,
+				com.Properties[0].Name,
+				com.Properties[0].Value,
+				com.Hashes[0].Alg,
+				com.Hashes[0].Content,
+				com.Licenses[0].Expression,
+				com.Purl,
+				depend.Ref,
+			); err != nil {
+				log.Fatal(err)
 			}
+			if err := tx.Commit(); err != nil {
+				log.Fatal(err)
+			}
+			stmt.Close()
 		}
 	}
-
 }
+
 func (c *DBClient) Close() {
 	_ = c.conn.Close()
 }
