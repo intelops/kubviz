0e87ac963767ed389f74f57d0bc4551afc2945ab
an1l4 <1995anila@gmail.com>
2023-08-23 13:50:29 +0530

    testingk8cluster38

--- agent/kubviz/trivy.go
+++ agent/kubviz/trivy.go
@@ -1,9 +1,10 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"log"
-	"strings"
+	exec "os/exec"
 	"sync"
 
 	"github.com/aquasecurity/trivy/pkg/k8s/report"
@@ -13,26 +14,48 @@ import (
 	"github.com/nats-io/nats.go"
 )
 
+// func RunTrivyK8sClusterScan(wg *sync.WaitGroup, js nats.JetStreamContext, errCh chan error) {
+// 	log.Println("*****started cluster scan")
+// 	defer wg.Done()
+// 	var report report.ConsolidatedReport
+// 	out, err := executeCommand("trivy k8s --report summary cluster --timeout 60m -f json -q --cache-dir /tmp/.cache")
+// 	parts := strings.SplitN(out, "{", 2)
+// 	if len(parts) <= 1 {
+// 		log.Println("No output from command", err)
+// 		errCh <- err
+// 		return
+// 	}
+// 	log.Println("Command logs", parts[0])
+// 	jsonPart := "{" + parts[1]
+// 	log.Println("First 200 lines output", jsonPart[:200])
+// 	log.Println("Last 200 lines output", jsonPart[len(jsonPart)-200:])
+// 	err = json.Unmarshal([]byte(jsonPart), &report)
+// 	if err != nil {
+// 		log.Printf("Error occurred while Unmarshalling json: %v", err)
+// 		errCh <- err
+// 	}
+// 	publishTrivyK8sReport(report, js, errCh)
+// }
+
 func RunTrivyK8sClusterScan(wg *sync.WaitGroup, js nats.JetStreamContext, errCh chan error) {
 	log.Println("*****started cluster scan")
 	defer wg.Done()
-	var report report.ConsolidatedReport
-	out, err := executeCommand("trivy k8s --report summary cluster --timeout 60m -f json -q --cache-dir /tmp/.cache")
-	parts := strings.SplitN(out, "{", 2)
-	if len(parts) <= 1 {
-		log.Println("No output from command", err)
-		errCh <- err
-		return
-	}
-	log.Println("Command logs", parts[0])
-	jsonPart := "{" + parts[1]
-	log.Println("First 200 lines output", jsonPart[:200])
-	log.Println("Last 200 lines output", jsonPart[len(jsonPart)-200:])
-	err = json.Unmarshal([]byte(jsonPart), &report)
+
+	command := "trivy k8s --report summary cluster --timeout 60m -f json -q --cache-dir /tmp/.cache"
+	out, err := executeCommandK8(command)
+
 	if err != nil {
-		log.Printf("Error occurred while Unmarshalling json: %v", err)
-		errCh <- err
+		log.Println("Error executing Trivy k8", err)
+		// Move on to the next image in case of an error
 	}
+
+	var report report.ConsolidatedReport
+	err = json.Unmarshal(out, &report)
+	if err != nil {
+		log.Printf("Error unmarshaling JSON data for k8 cluster %v", err)
+		// Move on to the next image in case of an error
+	}
+	log.Println("report", report)
 	publishTrivyK8sReport(report, js, errCh)
 }
 
@@ -49,4 +72,23 @@ func publishTrivyK8sReport(report report.ConsolidatedReport, js nats.JetStreamCo
 	}
 	log.Printf("Trivy report with ID:%s has been published\n", metrics.ID)
 	errCh <- nil
+}
+
+func executeCommandK8(command string) ([]byte, error) {
+	cmd := exec.Command("/bin/sh", "-c", command)
+	//cmd := exec.Command(command)
+	var outc, errc bytes.Buffer
+	cmd.Stdout = &outc
+	cmd.Stderr = &errc
+	log.Println("*******before ece command")
+	//stdout, err := cmd.Output()
+	err := cmd.Run()
+	log.Println("*******command ececuted")
+
+	if err != nil {
+		log.Println("Execute Command Error", err.Error())
+	}
+	// log.Println("*******output", outc.String(), errc.String())
+
+	return outc.Bytes(), err
 }
