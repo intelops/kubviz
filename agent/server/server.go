13c1542dcb152f7229f7f1a1dd83b9cfce6bee3f
vijeyash <vijeyash@gmail.com>
2023-09-10 14:52:21 +0530

    added liveness and pprof

---
+++ agent/server/server.go
@@ -0,0 +1,30 @@
+package server
+
+import (
+	"log"
+	"net/http"
+	_ "net/http/pprof"
+
+	"github.com/gin-gonic/gin"
+)
+
+func EnableProfile(r *gin.Engine) {
+	pprofGroup := r.Group("/debug/pprof")
+	{
+		pprofGroup.GET("/", gin.WrapH(http.DefaultServeMux))
+		pprofGroup.GET("/cmdline", gin.WrapH(http.DefaultServeMux))
+		pprofGroup.GET("/profile", gin.WrapH(http.DefaultServeMux))
+		pprofGroup.POST("/symbol", gin.WrapH(http.DefaultServeMux))
+		pprofGroup.GET("/symbol", gin.WrapH(http.DefaultServeMux))
+		pprofGroup.GET("/trace", gin.WrapH(http.DefaultServeMux))
+	}
+	r.GET("/liveness", func(c *gin.Context) {
+		c.String(http.StatusOK, "Alive")
+	})
+}
+
+func StartServer() {
+	r := gin.Default()
+	EnableProfile(r)
+	log.Fatal(r.Run(":6060"))
+}
