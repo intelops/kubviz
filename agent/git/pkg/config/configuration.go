670d9abda5065889d1de2a25ad802251a6cbb860
Shifna12Zarnaz <shifnazarnaz1205@gmail.com>
2023-09-14 17:05:45 +0530

    Enabled vault as optional

--- agent/git/pkg/config/configuration.go
+++ agent/git/pkg/config/configuration.go
@@ -5,7 +5,7 @@ package config
 
 //Config will have the configuration details
 type Config struct {
-	Enabled bool `envconfig:"ENABLED"`
+	Enabled bool `envconfig:"ENABLED" default:"true"`
 	CredIdentifier string `envconfig:"NATS_CRED_IDENTIFIER" default:"authToken"`
 	EntityName     string `envconfig:"NATS_ENTITY_NAME" default:"nats"`
 	NatsAddress string `envconfig:"NATS_ADDRESS"`
