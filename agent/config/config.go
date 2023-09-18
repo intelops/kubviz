9d34e81d4fb8191273b2c479bd3e8b424f3fe809
Nithunikzz <nithunithin2000@gmail.com>
2023-09-18 15:59:11 +0530

    test

--- agent/config/config.go
+++ agent/config/config.go
@@ -8,13 +8,13 @@ import (
 type AgentConfigurations struct {
 	SANamespace            string `envconfig:"SA_NAMESPACE" default:"default"`
 	SAName                 string `envconfig:"SA_NAME" default:"default"`
-	OutdatedInterval       string `envconfig:"OUTDATED_INTERVAL" default:"*/20 * * * *"`
+	OutdatedInterval       string `envconfig:"OUTDATED_INTERVAL" default:"@every 5m"`
 	GetAllInterval         string `envconfig:"GETALL_INTERVAL" default:"*/30 * * * *"`
 	KubeScoreInterval      string `envconfig:"KUBESCORE_INTERVAL" default:"*/40 * * * *"`
 	RakkessInterval        string `envconfig:"RAKKESS_INTERVAL" default:"*/50 * * * *"`
 	KubePreUpgradeInterval string `envconfig:"KUBEPREUPGRADE_INTERVAL" default:"*/60 * * * *"`
 	TrivyInterval          string `envconfig:"TRIVY_INTERVAL" default:"*/10 * * * *"`
-	SchedulerEnable        bool   `envconfig:"SCHEDULER_ENABLE" default:"false"`
+	SchedulerEnable        bool   `envconfig:"SCHEDULER_ENABLE" default:"true"`
 }
 
 func GetAgentConfigurations() (serviceConf *AgentConfigurations, err error) {
