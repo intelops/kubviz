7a0c02da74b8e4e3ac6c7339c6684086f99c80c7
Nithunikzz <nithunithin2000@gmail.com>
2023-10-31 21:12:49 +0530

    added tables

--- sql/20231031110543_update_event.up.sql
+++ sql/20231031110543_update_event.up.sql
@@ -1,11 +1,3 @@
 -- Change the engine to MergeTree
 ALTER TABLE events ENGINE = MergeTree();
 
--- Specify the new ORDER BY clause
-ALTER TABLE events ORDER BY ClusterName, EventTime;
-
--- Set a TTL policy on the EventTime column
-ALTER TABLE events TTL EventTime + INTERVAL 30 DAY;
-
--- Adjust the index granularity
-ALTER TABLE events SETTINGS index_granularity = 8192;
