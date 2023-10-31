c0a22ceeee215956a3f2afaca84595f9e3d7b5ca
Nithunikzz <nithunithin2000@gmail.com>
2023-10-31 23:28:58 +0530

    added tables

--- sql/20231031110543_update_event.up.sql
+++ sql/20231031110543_update_event.up.sql
@@ -1,27 +1,24 @@
--- Create a new table with the MergeTree engine
 CREATE TABLE IF NOT EXISTS new_events (
-	ClusterName String,
-	Id          String,
-	EventTime   DateTime('UTC'),
-	OpType      String,
-	Name        String,
-	Namespace   String,
-	Kind        String,
-	Message     String,
-	Reason      String,
-	Host        String,
-	Event       String,
-	FirstTime   String,
-	LastTime    String
+    ClusterName String,
+    Id          String,
+    EventTime   DateTime('UTC'),
+    OpType      String,
+    Name        String,
+    Namespace   String,
+    Kind        String,
+    Message     String,
+    Reason      String,
+    Host        String,
+    Event       String,
+    FirstTime   String,
+    LastTime    String
 ) ENGINE = MergeTree()
 ORDER BY ClusterName, EventTime
 TTL EventTime + INTERVAL 30 DAY
 SETTINGS index_granularity = 8192;
 
-
-
--- Copy data from the old table to the new one
 INSERT INTO new_events SELECT * FROM events;
 
--- Rename the tables to switch to the new table
 RENAME TABLE events TO old_events, new_events TO events;
+
+DROP TABLE old_events;
