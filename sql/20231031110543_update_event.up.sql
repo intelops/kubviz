1f257a55fe322e9739c3deb87b0c1e3bc1d731b1
Nithunikzz <nithunithin2000@gmail.com>
2023-11-01 10:51:39 +0530

    fix

--- sql/20231031110543_update_event.up.sql
+++ sql/20231031110543_update_event.up.sql
@@ -1,19 +1,5 @@
-CREATE TABLE IF NOT EXISTS new_events (
-    ClusterName String,
-    Id String,
-    EventTime DateTime('UTC'),
-    OpType String,
-    Name String,
-    Namespace String,
-    Kind String,
-    Message String,
-    Reason String,
-    Host String,
-    Event String,
-    FirstTime String,
-    LastTime String
-) ENGINE = MergeTree()
-ORDER BY ClusterName
-TTL EventTime + INTERVAL 30 DAY
-SETTINGS index_granularity = 8192;
-INSERT INTO new_events SELECT * FROM events;
+ALTER TABLE events
+    ENGINE = MergeTree()
+    ORDER BY (ClusterName, EventTime)
+    TTL EventTime + INTERVAL 10 MINUTE;
+   
