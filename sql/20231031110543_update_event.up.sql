ed24a0bdf94cc4b50315e84e9284ec486aac5d80
Nithunikzz <nithunithin2000@gmail.com>
2023-11-01 15:42:04 +0530

    changes event

--- sql/20231031110543_update_event.up.sql
+++ sql/20231031110543_update_event.up.sql
@@ -1,2 +1,17 @@
-ALTER TABLE events
-    ENGINE = MergeTree();
+-- Create a new table with the desired storage engine and schema
+CREATE TABLE new_events
+ENGINE = MergeTree()
+ORDER BY (ClusterName, EventTime)
+TTL EventTime TO EventTime + INTERVAL 10 MINUTE AS
+SELECT * FROM events;
+
+-- You may need to update indexes, constraints, and other properties as well.
+
+-- Copy data from the old table to the new one
+INSERT INTO new_events SELECT * FROM events;
+
+-- Drop the old table (if it's no longer needed)
+DROP TABLE events;
+
+-- Rename the new table to match the old table's name
+RENAME TABLE new_events TO events;
