571fc489d8184ebc48f7c0bee461b394d8aa7ddd
Nithunikzz <nithunithin2000@gmail.com>
2023-11-01 08:55:18 +0530

    added tables

--- sql/20231031110543_update_event.up.sql
+++ sql/20231031110543_update_event.up.sql
@@ -12,24 +12,11 @@ CREATE TABLE IF NOT EXISTS new_events (
     Event String,
     FirstTime String,
     LastTime String
-) CREATE TABLE IF NOT EXISTS new_events (
-    ClusterName String,
-    Id          String,
-    EventTime   DateTime('UTC'),
-    OpType      String,
-    Name        String,
-    Namespace   String,
-    Kind        String,
-    Message     String,
-    Reason      String,
-    Host        String,
-    Event       String,
-    FirstTime   String,
-    LastTime    String
 ) ENGINE = MergeTree()
-ORDER BY ClusterName, EventTime
-TTL EventTime + INTERVAL 30 DAY;
+ORDER BY ClusterName, EventTime;
 
+TTL EventTime + INTERVAL 30 DAY
+SETTINGS index_granularity = 8192;
 
 INSERT INTO new_events SELECT * FROM events;
 
