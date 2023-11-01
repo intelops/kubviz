e52def5c7fdad26bf730fbd5f9013e86f7bdb158
Nithunikzz <nithunithin2000@gmail.com>
2023-11-01 11:35:43 +0530

    fix

--- sql/20231031110543_update_event.up.sql
+++ sql/20231031110543_update_event.up.sql
@@ -1,5 +1,4 @@
 ALTER TABLE events
     ENGINE = MergeTree()
     ORDER BY (ClusterName, EventTime)
-    TTL EventTime + INTERVAL 10 MINUTE;
-   
+    TTL EventTime TO EventTime + INTERVAL 10 MINUTE;
