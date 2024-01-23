16fbfddc025f16dd71a982b15ef14100edf03486
Nithunikzz <nithunithin2000@gmail.com>
2024-01-23 20:37:07 +0530

    changes

--- client/pkg/storage/store.go
+++ client/pkg/storage/store.go
@@ -9,12 +9,6 @@ import (
 	"github.com/intelops/kubviz/client/pkg/config"
 )
 
-// const (
-// 	clickhouseDSN = "tcp://clickhouse-server:9000?username=default&password=&database=your_database"
-// 	s3Bucket      = "your-s3-bucket"
-// 	s3ObjectKey   = "exported_data.csv"
-// )
-
 // ExportExpiredData exports expired data from a specific table in ClickHouse to an external storage (S3 in this case).
 func ExportExpiredData(tableName string, conf *config.Config) error {
 	// Create ClickHouse database client
@@ -76,13 +70,6 @@ func ExportExpiredData(tableName string, conf *config.Config) error {
 		csvFile.WriteString(fmt.Sprintf("%s\n", rowData))
 	}
 
-	// Upload the CSV file to S3
-	// err = uploadToS3(fmt.Sprintf("exported_data_%s.csv", tableName))
-	// if err != nil {
-	// 	return fmt.Errorf("error uploading to S3: %v", err)
-	// }
-
-	// fmt.Printf("Data exported and uploaded to S3 successfully for table %s.\n", tableName)
 	return nil
 }
 func getTableColumns(db clickhouse.DBInterface, tableName string) (string, error) {
@@ -104,33 +91,3 @@ func getTableColumns(db clickhouse.DBInterface, tableName string) (string, error
 
 	return fmt.Sprintf("%s", columns), nil
 }
-
-// func uploadToS3(filename string) error {
-// 	sess, err := session.NewSession(&aws.Config{
-// 		Region: aws.String("your-region"),
-// 	})
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	file, err := os.Open(filename)
-// 	if err != nil {
-// 		return err
-// 	}
-// 	defer file.Close()
-
-// 	s3Client := s3.New(sess)
-// 	_, err = s3Client.PutObject(&s3.PutObjectInput{
-// 		Bucket: aws.String(s3Bucket),
-// 		Key:    aws.String(s3ObjectKey),
-// 		Body:   file,
-// 	})
-// 	if err != nil {
-// 		if awsErr, ok := err.(awserr.Error); ok {
-// 			fmt.Println("AWS Error:", awsErr.Code(), awsErr.Message())
-// 		}
-// 		return err
-// 	}
-
-// 	return nil
-// }
