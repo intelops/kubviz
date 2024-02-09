5de599bf132fccf5716cf543b83eb2a2de92565e
Nithunikzz <nithunithin2000@gmail.com>
2024-02-09 10:34:39 +0530

    added consumer as env

--- client/pkg/config/config.go
+++ client/pkg/config/config.go
@@ -7,4 +7,14 @@ type Config struct {
 	DBAddress          string `envconfig:"DB_ADDRESS"`
 	ClickHouseUsername string `envconfig:"CLICKHOUSE_USERNAME"`
 	ClickHousePassword string `envconfig:"CLICKHOUSE_PASSWORD"`
+	KetallConsumer     string `envconfig:"KETALL_EVENTS_CONSUMER"`
+	RakeesConsumer     string `envconfig:"RAKEES_METRICS_CONSUMER"`
+	OutdatedConsumer   string `envconfig:"OUTDATED_EVENTS_CONSUMER"`
+	DeprecatedConsumer string `envconfig:"DEPRECATED_API_CONSUMER"`
+	DeletedConsumer    string `envconfig:"DELETED_API_CONSUMER"`
+	KubvizConsumer     string `envconfig:"KUBVIZ_EVENTS_CONSUMER"`
+	KubscoreConsumer   string `envconfig:"KUBSCORE_CONSUMER"`
+	TrivyConsumer      string `envconfig:"TRIVY_CONSUMER"`
+	TrivyImageConsumer string `envconfig:"TRIVY_IMAGE_CONSUMER"`
+	TrivySbomConsumer  string `envconfig:"TRIVY_SBOM_CONSUMER"`
 }
