ff91e51997d7caa6387b0041915422d3bab2ea05
vijeyash <vijeyash@gmail.com>
2024-02-14 15:23:03 +0530

    minor change

--- model/kuberhealthy.go
+++ model/kuberhealthy.go
@@ -1,15 +1,17 @@
 package model
 
+import "time"
+
 // metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 type KuberhealthyCheckDetail struct {
-	CurrentUUID      string `json:"currentUUID"`
-	CheckName        string `json:"checkName"`
-	OK               uint8  `json:"ok"`
-	Errors           string `json:"errors"`
-	RunDuration      string `json:"runDuration"`
-	Namespace        string `json:"namespace"`
-	Node             string `json:"node"`
-	LastRun          string `json:"lastRun"`
-	AuthoritativePod string `json:"authoritativePod"`
+	CurrentUUID      string    `json:"currentUUID"`
+	CheckName        string    `json:"checkName"`
+	OK               uint8     `json:"ok"`
+	Errors           string    `json:"errors"`
+	RunDuration      string    `json:"runDuration"`
+	Namespace        string    `json:"namespace"`
+	Node             string    `json:"node"`
+	LastRun          time.Time `json:"lastRun"`
+	AuthoritativePod string    `json:"authoritativePod"`
 }
