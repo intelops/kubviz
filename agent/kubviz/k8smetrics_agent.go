562c7ecbfd554d0f862cc95404ac9fd19e8edd18
an1l4 <1995anila@gmail.com>
2024-02-15 11:43:07 +0530

    debugging

--- agent/kubviz/k8smetrics_agent.go
+++ agent/kubviz/k8smetrics_agent.go
@@ -20,14 +20,14 @@ import (
 
 	"github.com/intelops/kubviz/agent/config"
 	"github.com/intelops/kubviz/agent/kubviz/plugins/events"
-	//"github.com/intelops/kubviz/agent/kubviz/plugins/ketall"
-	//"github.com/intelops/kubviz/agent/kubviz/plugins/kubepreupgrade"
+	"github.com/intelops/kubviz/agent/kubviz/plugins/ketall"
+	"github.com/intelops/kubviz/agent/kubviz/plugins/kubepreupgrade"
 
 	"github.com/intelops/kubviz/agent/kubviz/plugins/kuberhealthy"
-	//"github.com/intelops/kubviz/agent/kubviz/plugins/kubescore"
+	"github.com/intelops/kubviz/agent/kubviz/plugins/kubescore"
 	"github.com/intelops/kubviz/agent/kubviz/plugins/outdated"
-	//"github.com/intelops/kubviz/agent/kubviz/plugins/rakkess"
-	//"github.com/intelops/kubviz/agent/kubviz/plugins/trivy"
+	"github.com/intelops/kubviz/agent/kubviz/plugins/rakkess"
+	"github.com/intelops/kubviz/agent/kubviz/plugins/trivy"
 	"github.com/intelops/kubviz/agent/kubviz/scheduler"
 
 	_ "k8s.io/client-go/plugin/pkg/client/auth/azure"
@@ -63,7 +63,7 @@ var (
 func main() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 	env := Production
-	//clusterMetricsChan := make(chan error, 1)
+	clusterMetricsChan := make(chan error, 1)
 	cfg, err := config.GetAgentConfigurations()
 	if err != nil {
 		log.Fatal("Failed to retrieve agent configurations", err)
@@ -127,7 +127,7 @@ func main() {
 		}
 	}()
 
-	//go events.PublishMetrics(clientset, js, clusterMetricsChan)
+	go events.PublishMetrics(clientset, js, clusterMetricsChan)
 	if cfg.KuberHealthyEnable {
 		go kuberhealthy.StartKuberHealthy(js)
 	}
@@ -135,20 +135,20 @@ func main() {
 	collectAndPublishMetrics := func() {
 		err := outdated.OutDatedImages(config, js)
 		events.LogErr(err)
-		// err = kubepreupgrade.KubePreUpgradeDetector(config, js)
-		// events.LogErr(err)
-		// err = ketall.GetAllResources(config, js)
-		// events.LogErr(err)
-		// err = rakkess.RakeesOutput(config, js)
-		// events.LogErr(err)
-		// err = trivy.RunTrivySbomScan(config, js)
-		// events.LogErr(err)
-		// err = trivy.RunTrivyImageScans(config, js)
-		// events.LogErr(err)
-		// err = trivy.RunTrivyK8sClusterScan(js)
-		// events.LogErr(err)
-		// err = kubescore.RunKubeScore(clientset, js)
-		// events.LogErr(err)
+		err = kubepreupgrade.KubePreUpgradeDetector(config, js)
+		events.LogErr(err)
+		err = ketall.GetAllResources(config, js)
+		events.LogErr(err)
+		err = rakkess.RakeesOutput(config, js)
+		events.LogErr(err)
+		err = trivy.RunTrivySbomScan(config, js)
+		events.LogErr(err)
+		err = trivy.RunTrivyImageScans(config, js)
+		events.LogErr(err)
+		err = trivy.RunTrivyK8sClusterScan(js)
+		events.LogErr(err)
+		err = kubescore.RunKubeScore(clientset, js)
+		events.LogErr(err)
 	}
 
 	collectAndPublishMetrics()
